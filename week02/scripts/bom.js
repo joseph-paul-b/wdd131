efd9db2860e5d7b693f93e47df8ef2e5936a00bd
joseph-paul-b <pualjoseph11x@gmail.com>
2026-09-12 17:48:28 +0100

    Add DOM event handling to BOM application

---
+++ week02/scripts/bom.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('#favchap');
+
+const button = document.querySelector('button');
+
+const list = document.querySelector('ul');
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        list.append(li);
+
+        input.value = '';
+
+        input.focus();
+    }
+});
